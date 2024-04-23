7cc107855ecfafe7847f6e4f983c53b63f0f70d9
moonman13 <jfm9413@nyu.edu>
2024-04-23 18:57:11 -0400

    Sync push

---
+++ Assets/Scripts/Jack/Wave.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Wave
+{
+	public int preDelay;
+	public int numOrders; //0 thru 4
+	public int orderTime; 
+}
